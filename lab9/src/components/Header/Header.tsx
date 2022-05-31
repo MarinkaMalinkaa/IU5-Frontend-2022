da0d2a2432707b237bc611346ec8a31325160204
MarinkaMalinkaa <2002_marina@mail.ru>
2022-05-31 10:30:25 +0300

    done)

--- lab9/src/components/Header/Header.tsx
+++ lab9/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import "./Header.css"
 export const Header: React.FC = () => {
     return (
         <header>
-            <h2>Ого, тут что можно найти аккаунт в GitHub</h2>
+            <h2>Ого, тут что можно найти аккаунт в GitHub!</h2>
         </header>
     );
 }
