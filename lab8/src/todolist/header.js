0f5d9784544afb4351b267246b86470dc6d2feac
MarinkaMalinkaa <2002_marina@mail.ru>
2022-05-31 14:09:30 +0300

    lab8

---
+++ lab8/src/todolist/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+export function Header() {
+    return (
+        <header className='navbar'>
+            <div>Список супер важных дел </div>
+        </header>
+    );
+}
